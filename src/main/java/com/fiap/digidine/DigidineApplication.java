ec9dd112ee89ba5421d1d680b611053c901b634d
Bruno Gago <92176548+BrunoGago@users.noreply.github.com>
2024-07-16 22:20:15 -0300

    Inicialização de repositório

---
+++ src/main/java/com/fiap/digidine/DigidineApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.digidine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigidineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigidineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/digidine/DigidineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.digidine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigidineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
